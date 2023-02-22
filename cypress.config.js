8116dccb1d261fb4711e32d31b5ca2cdcb18710a
anglezh <inbzhanglei@163.com>
2023-02-22 15:08:47 +0800

    eslint fix

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,9 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      this.pageLoadTimeout=1000000
-    },
+    // setupNodeEvents(on, config) {
+    //   // implement node event listeners here
+    // },
   },
-});
+})
